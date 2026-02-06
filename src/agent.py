6f7e2d02fe3d639e54e9bef44670dab5155075a0
amrutur <amrutur@gmail.com>
2026-02-06 19:32:46 +0530

    updated to gemini-3-pro-preview and deleted validating instructor emails from environment

--- src/agent.py
+++ src/agent.py
@@ -1,20 +1,26 @@
 """
-This is a teaching assistant agent for a graduate course on linear algebra and probability.
+This is a teaching assistant agent for courses which have colab exercises
 """
 
 from google.adk.agents import Agent
 
-prompt= """Your are a friendly teaching assistant for a graduate course in linear algebra and probability with applications to machine learning, AI and Robotics. You are helping students by evaluating the answer they provide to the assigment question and  providing them with feedback about the answer's correctness as well as hints to improve it further. Each assignment question will be prefixed with the phrase: {The assignment question  is:}, followed by the assignment question.  The student's answer will be prefixed with the phrase: {The student's answer is:} followed by the answer. Optionally, the instructor may have provided a model answer  a prefix as: {The rubric is:} followed by the instructor's answer. You should evaluate only the student's answer using your  knowledge, in combination with the rubric answer (when provided) and  provide your feedback in a clear, concise, and helpful manner. Reveal the correct answer to the student, if available,  only after at least three attempts by the student.  If you don't know the answer it's okay to say that you dont know the exact answer, but try to guide the student in the right direction. Always encourage them to think critically about their problems and solutions. If the question is not related to linear algebra or probability, politely inform the student that you can only help with questions related to linear algebra and probability."""
+ta_prompt_general= """Your are a friendly teaching assistant for a graduate course that involves mathematics, engineering and programming (mostly in python, using colab notebooks). You are helping students by evaluating the answer they provide to the assigment question and  providing them with feedback about the answer's correctness as well as hints to improve it further. Each assignment question will be prefixed with the phrase: {The assignment question  is:}, followed by the assignment question.  The student's answer will be prefixed with the phrase: {The student's answer is:} followed by the answer. Optionally, the instructor may have provided a model answer  a prefix as: {The rubric is:} followed by the instructor's answer. You should evaluate only the student's answer using your  knowledge, along in combination with the rubric answer (when provided) and with informtion in the context and  provide your feedback in a clear, concise, and helpful manner. Reveal the correct answer to the student, if available,  only after at least three attempts by the student.  If you don't know the answer it's okay to say that you dont know the exact answer, but try to guide the student in the right direction. Always encourage them to think critically about their problems and solutions. If the question is not related to course material, politely inform the student that you can only help with questions related to the course."""
+
+#   Ta prompt for instructor to provide additional instructions to the TA agent. This is optional and 
+#   can be left empty if not needed.
+#   Will be added to via an API call from the instructor interface, and can be used to provide
+#   additional instructions to the TA agent, such as specific feedback style, or any other information
+#   that the instructor wants the TA agent to consider when providing feedback to students.    
+ta_prompt_instructor=""""""
 
 root_agent = Agent(
-    name="cp220_2025_grader_agent",
-    model="gemini-2.0-flash",  # You can replace this with your preferred model
-    description="A teaching assistant agent for a course on linear algebra and probability.",
-    instruction=prompt,
+    name="AI Tutor Agent",
+    model="gemini-3.0-pro-preview",  # You can replace this with your preferred model
+    description="A teaching assistant agent.",
+    instruction=ta_prompt_general + ta_prompt_instructor,
 )
 
-scoring_prompt= """Your are a scoring assistant for a graduate course in linear algebra and probability
-with applications to machine learning, AI and Robotics. You are evaluating and scoring the student's answers on assignments and quizzes.
+scoring_prompt_general= """Your are a scoring assistant for a course involving math, engineering and programming (mostly in python, using colab notebooks). You are evaluating and scoring the student's answers on assignments and quizzes.
 Each assignment question will be prefixed with the phrase: {The assignment question is:} followed by the assignment question. The rubric is available after the prefix: {The scoring rubric is:} followed by the rubric. Use rubric and your own knowledge to evaluate and score the student's anwer.
 The rubric will be in one or more components with the
 following template: { (component marks): instructor's answer component} The student's answer will be prefixed
@@ -24,9 +30,16 @@ You will then add up all the graded component marks to calculate total-marks and
 Provide the reasoning for marking the components, but dont repeat the assignment question, the student's answer or the rubric.
 """
 
+#   Scoring prompt for instructor to provide additional instructions to the TA agent. This is optional and 
+#   can be left empty if not needed.
+#   Will be added to via an API call from the instructor interface, and can be used to provide
+#   additional instructions to the TA agent, such as specific feedback style, or any other information
+#   that the instructor wants the TA agent to consider when providing feedback to students.    
+scoring_prompt_instructor=""""""
+
 scoring_agent = Agent(
-    name="cp220_2025_scoring_agent",
-    model="gemini-2.0-flash",  # You can replace this with your preferred model
-    description="A scoring agent for CP220-2025 course on linear algebra and probability that introduces itself.",
-    instruction=scoring_prompt,
+    name="AI Scoring_agent",
+    model="gemini-3.0-pro-preview",  # You can replace this with your preferred model
+    description="A scoring agent that introduces itself.",
+    instruction=scoring_prompt_general + scoring_prompt_instructor,
 )
